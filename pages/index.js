1f742e081139b5f688249d5efcdbb030f346bbe6
Paulo Vitor <93552279+Wpnnt@users.noreply.github.com>
2025-07-04 13:18:53 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,9 +43,7 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Paulo Vitor
-            </h2>
+            
             <div className="mx-auto bg-gradient-to-b from-[#0505f5] rounded-full w-80 h-80 relative overflow-hidden mt-3 md:h-96 md:w-96">
               <Image
                 src={wpnnt}
@@ -54,6 +52,9 @@ export default function Home() {
                 alt="Profile picture"
               />
             </div>
+                  <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Paulo Vitor
+            </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Junior Developer
             </h3>
